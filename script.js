b36746a5e016fec21417fbfeba85e4664c227872
HoHuDooX <dohuuhoai285@gmail.com>
2024-10-29 22:11:00 +0700

    update

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ document
     const username = document.getElementById("maquanli").value;
     const password = document.getElementById("password").value;
     const usernamePattern = /^[a-zA-Z0-9._]{4,20}$/;
-    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const passwordPattern = /^.{8,}$/;
 
     if (usernamePattern.test(username) && passwordPattern.test(password)) {
       window.location.href = "dashboard.html";
     } else {
       document.getElementById("message").innerText =
-        "*Tên người dùng hoặc mật khẩu không đúng!";
+        "Tên người dùng hoặc mật khẩu không đúng!";
     }
   });
 
