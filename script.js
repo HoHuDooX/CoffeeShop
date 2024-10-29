14a94d911651102e50de1aa6a652dd9ef11ccda0
HoHuDooX <dohuuhoai285@gmail.com>
2024-10-30 01:19:00 +0700

    done

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ document
     const username = document.getElementById("maquanli").value;
     const password = document.getElementById("password").value;
     const usernamePattern = /^[a-zA-Z0-9._]{4,20}$/;
-    const passwordPattern =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const passwordPattern = /^.{8,}$/;
 
     if (usernamePattern.test(username) && passwordPattern.test(password)) {
       window.location.href = "dashboard.html";
@@ -14,14 +13,4 @@ document
       document.getElementById("message").innerText =
         "Tên người dùng hoặc mật khẩu không đúng!";
     }
-  });
-
-document
-  .getElementById("togglePassword")
-  .addEventListener("click", function () {
-    const passwordInput = document.getElementById("password");
-    const isPasswordHidden = passwordInput.getAttribute("type") === "password";
-    passwordInput.setAttribute("type", isPasswordHidden ? "text" : "password");
-    this.classList.toggle("fa-eye-slash", !isPasswordHidden);
-    this.classList.toggle("fa-eye", isPasswordHidden);
   });
