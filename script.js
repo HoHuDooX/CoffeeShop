ff7d1aff1c4174bea8b2a2818923f2ebe8e5a48f
HoHuDooX <dohuuhoai285@gmail.com>
2024-11-01 23:08:40 +0700

    add select role

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ document
   .getElementById("loginForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    const username = document.getElementById("maquanli").value;
+    const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    const role = document.getElementById("role").value;
     const usernamePattern = /^[a-zA-Z0-9._]{4,20}$/;
     const passwordPattern = /^.{8,}$/;
-
     if (usernamePattern.test(username) && passwordPattern.test(password)) {
-      window.location.href = "dashboard.html";
+      if (role === "manager") {
+        window.location.href = "dashboard.html";
+      } else if (role === "employee") {
+        window.location.href = "employee_menu.html";
+      }
     } else {
       document.getElementById("message").innerText =
         "Tên người dùng hoặc mật khẩu không đúng!";
     }
   });
+
 document
   .getElementById("togglePassword")
   .addEventListener("click", function () {
